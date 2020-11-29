c4ee3f33adea7a986e9ab6e3b28534677a2e4bc3
ajjTest <1120837962@qq.com>
2020-11-29 18:08:44 +0800

    https://github.com/ajuana/juana.git
    Signed-off-by: ajjTest <1120837962@qq.com>

---
+++ test_case/test_baidu.py
@@ -0,0 +1,4 @@
+import requests
+url='https://www.baidu.com/'
+r=requests.get(url)
+print(r.text)
---
+++ test_case/test_case_01.py
@@ -0,0 +1,7 @@
+def test_1():
+    print("1111")
+    assert 1==1
+def test_2():
+    assert 1==2
+def test_3():
+    assert 1==3
